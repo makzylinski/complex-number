780497d8d427d5a32438fdf6b446a80237cddd49
Maks <makzylinski@gmail.com>
2024-07-31 12:48:32 +0200

    numbers

---
+++ src/ComplexNumber.java
@@ -0,0 +1,34 @@
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+
+        System.out.println("Result ");
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber num) {
+        this.real -= num.real;
+        this.imaginary -= num.imaginary;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ComplexNumber complexNumber = new ComplexNumber(1, 4);
+        complexNumber.add(1, 4);
+        complexNumber.add(1, 4);
+    }
+}
